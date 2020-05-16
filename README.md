75d1a51d83abff42e57a2ceadee2f652c83a7465
Chris Connelly <chris@connec.co.uk>
2020-05-16 20:20:09 +0100

    feat: Basic working CLI
    The CLI allows a single manifest to be passed with --filename and a
    single config to be passed with --config.

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # `kubetpl`
 
 Templatisation for Kubernetes manifests
+
+## Overview
+
+`kubetpl` is a CLI tool for interpolating Kubernetes manifests using the Go templating language.
+The supported input and output format is intended to facilitate usage in a pipeline with `kubectl apply -f -`.
+
+## Usage
+
+The CLI is largely self documenting:
+
+```sh
+kubetpl --help
+```
+
+### Basic usage
+
+```sh
+kubetpl --filename kubernetes.yaml --config dev.yaml \
+  | kubectl apply -f -
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,95 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashMap;
+use std::error::Error;
+use std::fs::{self};
+
+use structopt::StructOpt;
+use yaml_rust::{Yaml, YamlLoader};
+
+/// Templatisation for Kubernetes manifests
+#[derive(Debug, StructOpt)]
+struct Command {
+    /// The path to a Kubernetes manifest template.
+    #[structopt(long, short)]
+    filename: String,
+
+    /// The path to a configuration YAML file.
+    ///
+    /// The YAML file should contain a single document and any mappings must have string keys.
+    #[structopt(long, short)]
+    config: String,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let cmd = Command::from_args();
+
+    let manifest = load_manifest(&cmd.filename)?;
+    let config = load_config(&cmd.config)?;
+
+    let context = yaml_to_gtmpl(config)?;
+    let result = gtmpl::template(&manifest, context)?;
+    print!("{}", result);
+
+    Ok(())
+}
+
+fn load_manifest(path: &str) -> Result<String, Box<dyn Error>> {
+    Ok(fs::read_to_string(path)?)
+}
+
+fn load_config(path: &str) -> Result<Yaml, Box<dyn Error>> {
+    let mut config = YamlLoader::load_from_str(&fs::read_to_string(path)?)?;
+    if config.len() == 1 {
+        Ok(config.remove(0))
+    } else {
+        Err(format!(
+            "Expected {} to contain a single YAML document but found {}",
+            path,
+            config.len()
+        )
+        .into())
+    }
+}
+
+/// Convert a [`yaml_rust::Yaml`] value into a [`gtmpl::Value`].
+///
+/// The implementation is based on the `into_*` methods on `yaml_rust::Yaml`, rather than matching
+/// to convert between the enums. This is due to some non-trivial incompatibilities such as
+/// `Yaml::Real` storing its value as a `String` and the resolution of `Yaml::Alias` values.
+fn yaml_to_gtmpl(input: Yaml) -> Result<gtmpl::Value, Box<dyn Error>> {
+    use gtmpl::Value::*;
+    if input.as_bool().is_some() {
+        Ok(Bool(input.into_bool().unwrap()))
+    } else if input.as_f64().is_some() {
+        Ok(Number(input.into_f64().unwrap().into()))
+    } else if input.as_hash().is_some() {
+        let input = input.into_hash().unwrap();
+        let mut output = HashMap::with_capacity(input.len());
+        input
+            .into_iter()
+            .try_for_each::<_, Result<_, Box<dyn Error>>>(|(key, value)| {
+                let key = match key.into_string() {
+                    Some(key) => key,
+                    None => return Err("non-string keys are not supported".into()),
+                };
+                output.insert(key, yaml_to_gtmpl(value)?);
+                Ok(())
+            })?;
+        Ok(Map(output))
+    } else if input.as_i64().is_some() {
+        Ok(Number(input.into_i64().unwrap().into()))
+    } else if input.as_str().is_some() {
+        Ok(String(input.into_string().unwrap()))
+    } else if input.as_vec().is_some() {
+        let input = input.into_vec().unwrap();
+        let mut output = Vec::with_capacity(input.len());
+        input
+            .into_iter()
+            .try_for_each::<_, Result<_, Box<dyn Error>>>(|value| {
+                output.push(yaml_to_gtmpl(value)?);
+                Ok(())
+            })?;
+        Ok(Array(output))
+    } else {
+        Err(format!("YAML value cannot be used in Go templates: {:?}", input).into())
+    }
 }
